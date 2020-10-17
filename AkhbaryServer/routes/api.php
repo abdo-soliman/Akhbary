339d0652737193eceb854bd512b19c8e8f0436e0
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2020-10-18 00:41:03 +0200

    added routes and JWT Middleware

--- AkhbaryServer/routes/api.php
+++ AkhbaryServer/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('closed', 'DataController@closed');
 });
