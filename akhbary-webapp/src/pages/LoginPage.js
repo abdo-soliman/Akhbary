e71b73b68afcb148d185adca35f5d0b25380953b
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2020-10-18 20:06:01 +0200

    added title compnent

--- akhbary-webapp/src/pages/LoginPage.js
+++ akhbary-webapp/src/pages/LoginPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import "../styles/Login.css"
+import Title from "../components/Title";
 import Button from "../components/Button";
 
 export default class LoginPage extends Component {
     render() {
         return (
             <div className="form">
+                <Title title="Login" />
                 <Button mode="flat" label="Login" />
             </div>
         );
