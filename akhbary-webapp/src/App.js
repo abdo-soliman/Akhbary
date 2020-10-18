e6ff711377a7d6c3bcd2715e7d634cfd7ed19a97
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2020-10-18 15:23:25 +0200

    added dir structure and navbar

--- akhbary-webapp/src/App.js
+++ akhbary-webapp/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import MainNavigator from "./navigation";
+import "./styles/app.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={require("./logo.png")} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <MainNavigator />
+                <div className="container">
+                    <p>some test text</p>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
